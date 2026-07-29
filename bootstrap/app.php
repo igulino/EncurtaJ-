72ccde1eedc5bf5c590277c67b43f7c0d7da5f17
igão <ligor7250@gmail.com>
2026-07-29 15:03:11 -0300

    without loglivewire

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,10 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->web(append: [
-            LogLivewireRequests::class,
-        ]);
-        //
+    
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
